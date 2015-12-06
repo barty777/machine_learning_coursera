6eae37be520d873d71d7b20443136b38a65ccdac
Bartol <freskura.bartol@gmail.com>
2015-12-06 20:16:24 +0100

    First Exercise, finished mandatory part

---
+++ lab_1/ex1/featureNormalize.m
@@ -0,0 +1,58 @@
+function [X_norm, mu, sigma] = featureNormalize(X)
+%FEATURENORMALIZE Normalizes the features in X 
+%   FEATURENORMALIZE(X) returns a normalized version of X where
+%   the mean value of each feature is 0 and the standard deviation
+%   is 1. This is often a good preprocessing step to do when
+%   working with learning algorithms.
+
+% You need to set these values correctly
+X_norm = X;
+mu = zeros(1, size(X, 2));
+sigma = 0;
+
+meanX=0;
+feature1= zeros(1, size(X, 1));
+feature2= zeros(1, size(X, 2));
+mu1= zeros(1, size(X, 2));
+mu2= zeros(1, size(X, 2));
+sigma1 = 0;
+sigma2=0;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: First, for each feature dimension, compute the mean
+%               of the feature and subtract it from the dataset,
+%               storing the mean value in mu. Next, compute the 
+%               standard deviation of each feature and divide
+%               each feature by it's standard deviation, storing
+%               the standard deviation in sigma. 
+%
+%               Note that X is a matrix where each column is a 
+%               feature and each row is an example. You need 
+%               to perform the normalization separately for 
+%               each feature. 
+%
+% Hint: You might find the 'mean' and 'std' functions useful.
+%       
+
+
+meanX = mean(X(:,1));
+mu1 = X(:,1)-meanX;
+sigma1 = std(X(:,1));
+feature1 = mu1/sigma1;
+
+meanX = mean(X(:,2));
+mu2 = X(:,2)-meanX;
+sigma2 = std(X(:,2));
+feature2 = mu2/sigma2;
+
+X_norm= [feature1 , feature2];
+mu=mu2;
+sigma=sigma1;
+
+
+
+
+
+% ============================================================
+
+end
---
+++ lab_1/ex1/gradientDescent.m
@@ -0,0 +1,44 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+delta1 = 0;
+delta2=0;
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+
+
+% Calculate sum for theta(1) and theta(2)
+for i = 1:m
+  delta1 = delta1 + (theta'*X(i,:)'-y(i, : ))*X(i,1);
+  delta2 = delta2 + (theta'*X(i,:)'-y(i, : ))*X(i,2);
+endfor
+
+delta1= delta1*alpha/m;
+delta2= delta2*alpha/m;
+
+deltaVector = [delta1 ; delta2];
+
+theta = theta - deltaVector;
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);
+endfor
+
+J_history(iter) = computeCost(X, y, theta)
+
+end
